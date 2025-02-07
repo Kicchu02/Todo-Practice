8e910a16f831d9a498cbf18b9aea8579e38e0dec
Kicchu02 <kicchu.2002@gmail.com>
2025-02-08 01:50:48 +0530

    Implement MainContent.tsx [WIP]

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { colors } from "../colorConstants";
 
 export const MainContent = (): React.ReactElement => {
-  return <>Main content</>;
+  return (
+    <div className={`py-6 gap-4 bg-[${colors.background}]`}>
+      <div className="border-t border-white py-1 flex flex-col items-center">
+        <p
+          style={{
+            fontSize: "13px",
+            fontWeight: 500,
+            lineHeight: "16px",
+            textAlign: "left",
+            textUnderlinePosition: "from-font",
+            textDecorationSkipInk: "none",
+            color: "#142E159E",
+          }}
+        >
+          To Do
+        </p>
+
+        <div className="flex flex-row items-center bg-gradient-to-r from-transparent to-transparent via-[rgba(53,121,55,0.1)]">
+          <div className="pt-40 px-20 gap-48">
+            <p
+              style={{
+                fontSize: "15px",
+                fontWeight: 400,
+                lineHeight: "20px",
+                textAlign: "left",
+                textUnderlinePosition: "from-font",
+                textDecorationSkipInk: "none",
+                color: "#1B281BB8",
+              }}
+            >
+              Add A Task
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
